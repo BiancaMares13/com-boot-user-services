3785c8f230647883e775ed7f8118fe7bb87d575d
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-03-03 09:11:48 +0200

    re-initial commit

---
+++ src/main/java/com/boot/user/services/SpringUserServicesApplication.java
@@ -0,0 +1,14 @@
+package com.boot.user.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserServicesApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/boot/user/services/SpringUserServicesTests.java
@@ -0,0 +1,13 @@
+package com.boot.user.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserServicesTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
